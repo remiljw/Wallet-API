8ab2944826ddbe29523c892907e76973a2e9d65f
Michael Ajanaku <remiljw@gmail.com>
2021-02-10 00:27:58 +0100

    Register Models

--- Wallets/admin.py
+++ Wallets/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import User, Wallet, Transaction_History
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Wallet)
+admin.site.register(Transaction_History)
+
+admin.site.unregister(Group)
