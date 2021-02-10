8bf394d1585c5680cff7a0873170779d274692d0
Michael Ajanaku <remiljw@gmail.com>
2021-02-10 17:21:42 +0100

    Heroku Config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Wallet_API.wsgi --log-file -
--- Wallet_API/settings.py
+++ Wallet_API/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 AUTH_USER_MODEL = 'Wallets.User'
 JWT_AUTH = {
     'JWT_ENCODE_HANDLER':
