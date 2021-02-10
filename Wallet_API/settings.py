d56332e027c9b474030a15d134e82dd982eb19d9
Michael Ajanaku <remiljw@gmail.com>
2021-02-10 17:29:05 +0100

    static settings

--- Wallet_API/settings.py
+++ Wallet_API/settings.py
@@ -137,8 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Wallets/static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Wallets/static'),)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 AUTH_USER_MODEL = 'Wallets.User'
 JWT_AUTH = {
