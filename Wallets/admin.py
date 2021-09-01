206dfc1662c3822e316e20da58e26768b091053c
Michael Ajanaku <remiljw@gmail.com>
2021-09-01 11:12:36 +0100

    fix admin

--- Wallets/admin.py
+++ Wallets/admin.py
@@ -6,14 +6,14 @@ from .models import User, Wallet, TransactionHistory
 class TransactionHistoryAdmin(admin.ModelAdmin):
     readonly_fields=("reference_number", "trans_type", "sender", "recipient", "amount", "details", "time")
     list_display = ("reference_number", "trans_type", "sender", "recipient", "amount", "time")
-    search_fields = ("trans_type", "sender", "recipient", "amount")
+    search_fields = ("trans_type", "sender__account_no", "recipient__account_no", "amount")
     list_filter = ("trans_type",)
     list_per_page = 50
 
 class WalletsAdmin(admin.ModelAdmin):
     readonly_fields=("account_no", "owner", "balance", "date_created", "date_modified")
     list_display = ("account_no", "owner", "balance")
-    search_fields = ("account_no", "owner")
+    search_fields = ("account_no", "owner__email")
     list_per_page = 50
 
 admin.site.register(User)
